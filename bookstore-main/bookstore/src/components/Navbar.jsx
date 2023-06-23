c63a52138244b04f1fe52c14cd6eccad134706a7
karuna123761 <karuna22parihar@gmail.com>
2023-06-23 12:58:39 +0530

    hello

--- bookstore-main/bookstore/src/components/Navbar.jsx
+++ bookstore-main/bookstore/src/components/Navbar.jsx
@@ -127,9 +127,11 @@ function NavList() {
       </Typography>
 
       <Typography as="a"  variant="small" className="font-normal">
+        <Link to="/blogpage">
         <ListItem className="flex items-center gap-2 py-2 pr-10">
           BLOGS
         </ListItem>
+        </Link>
       </Typography>
      
       <NavListMenu />
